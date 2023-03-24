9aef5672f9bf9be9693d7659b8e84a23189e6b15
Samet Kamgul <sametkamgul@gmail.com>
2023-03-25 00:55:41 +0300

    inital implementation
    - home, about, notfound pages
    - router implementation
    - tailwindcss, daisyui usage
    - cleanup unnecessary react files

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h1 className='text-6xl mb-4'>Github Finder</h1>
+            <p>this is a github finder app about page.</p>
+        </div>
+    );
+}
+
+export default About;
