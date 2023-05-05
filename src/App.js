1285cba8683f947759b089271fd3e48576eef0c4
Samet Kamgul <sametkamgul@gmail.com>
2023-05-06 00:08:27 +0300

    - User page implementation, github repos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
+import User from './pages/User';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
@@ -16,10 +17,11 @@ function App() {
                     <div className='flex flex-col justify-between h-screen'>
                         <Navbar />
                         <main className='container mx-auto px-3 pb-12'>
-                            <Alert/>
+                            <Alert />
                             <Routes>
                                 <Route path='/' element={<Home />} />
                                 <Route path='/about' element={<About />} />
+                                <Route path='/user/:login' element={<User />} />
                                 <Route
                                     path='/notfound'
                                     element={<NotFound />}
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,6 +9,8 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
     const initialState = {
         users: [],
+        user: {},
+        repos: [],
         loading: false,
     };
 
@@ -37,6 +39,51 @@ export const GithubProvider = ({ children }) => {
         });
     };
 
+    // get single user
+    const getUser = async (login) => {
+        setLoading();
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`,
+            },
+        });
+
+        if (response.status === 404) {
+            window.location = '/notfound';
+        } else {
+            const data = await response.json();
+
+            dispatch({
+                type: 'GET_USER',
+                payload: data,
+            });
+        }
+    };
+
+    // get user repos
+    const getUserRepos = async (login) => {
+        setLoading();
+
+        const params = new URLSearchParams({
+            sort: "created",
+            per_page: 10
+        });
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`,
+            },
+        });
+
+        const data = await response.json();
+
+        dispatch({
+            type: 'GET_REPOS',
+            payload: data,
+        });
+    };
+
     // set loading
     const setLoading = () => {
         dispatch({
@@ -55,9 +102,13 @@ export const GithubProvider = ({ children }) => {
         <GithubContext.Provider
             value={{
                 users: state.users,
+                user: state.user,
+                repos: state.repos,
                 loading: state.loading,
                 searchUsers,
                 clearUsers,
+                getUser,
+                getUserRepos,
             }}
         >
             {children}
